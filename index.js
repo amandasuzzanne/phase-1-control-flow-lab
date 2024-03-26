5871a01304ae146281af0bbf5b4a72029dc857a6
amandasuzzanne <amandasuzanne12@gmail.com>
2024-03-26 14:32:18 +0300

    Complete control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+    let message;
+
+    if (feet <= 400){
+      message = 'This one is on me!';
+    } else if (feet > 400 && feet <= 2000){
+      message = 'That will be twenty bucks.';
+    } else if (feet > 2000 && feet <= 2500){
+      message = 'I will gladly take your thirty bucks.';
+    } else {
+      message = 'No can do.';
+    }
+    return message;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  let message;
+
+  message = (city === "NYC") ? 'Ok, sounds good.' : 'No go.';
+
+  return message;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let message;
+
+  switch (tip) {
+    case 'generous':
+      message = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      message = 'Thank you.';
+      break;
+    default:
+      message = 'Bye.';
+  }
+  return message;
 }
